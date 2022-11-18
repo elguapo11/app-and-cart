4ed0b532075be5b17d85ed99a8df8c0235929eb2
Benny Mendoza <bennybc9387@gmail.com>
2022-11-18 09:35:02 -0700

    restarting for the millionth time, fixed file structure finally

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Look at me figuring shit out. I'm a developer at a large company, and I'm successful at anything I set my mind to</p>
+    </div>
+  );
+}
+
+export default App;
