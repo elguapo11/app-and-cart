325e25a0535efdc01ef374e04255306facbc2450
Benny Mendoza <bennybc9387@gmail.com>
2022-12-21 21:31:06 -0700

    used fill to color ellipse

--- client/src/purplerain.js
+++ client/src/purplerain.js
@@ -8,20 +8,35 @@ import './purplerain.css';
 export default class Rain extends Component {
   x = 50 
   y = 50
+  z = 50
 
   setup = (p5) => {
-    p5.createCanvas(300, 300)
+    p5.createCanvas(500, 500)
+
+
+
+
+
     
   
   }
   draw = p5 => {
-    p5.background('rgba(0,0,0, 0.25)')
-    p5.ellipse(this.x, this.y, 150, 150)
-    this.x++  
+    p5.ellipse(this.x, this.y, this.z)
+    p5.ellipse('50,50,80,80')
+    p5.fill('rgba(94, 5, 51, 0.8)')
+    this.x++
+    this.y++  
+    this.z++
+    
+    
+
 
 
  
   }
+   
+  
+  
 
   render() {
     return (
