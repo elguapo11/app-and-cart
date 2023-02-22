52c3dedf9b593406c9c04b8d0d13f9a8d74263bc
Benny Mendoza <bennybc9387@gmail.com>
2023-02-21 21:32:52 -0700

    removed unneccesary text

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h3>Look at me figuring shit out plus Nate</h3>
+      <h3>Look at me figuring shit out</h3>
       {console.log("hello World")}
     </div>
   );
