79ce0045aa3ca558b5cfc139de014c725d522fa3
Benny Mendoza <bennybc9387@gmail.com>
2023-03-09 20:10:28 -0700

    adding notes to my project for understanding

--- client/src/Todo.js
+++ client/src/Todo.js
@@ -6,23 +6,36 @@ function TodoList() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    // Here we are saying do not refresh the page when until event is over
     if (newTodo.trim() === '') return;
+    //If the field is empty then just return, end the function.
     setTodos([...todos, { text: newTodo }]);
+    //here we are doing a couple of things. First, we are setting  'setTodos' to whatever was entered in todos and making
+    //text be an object and setting 'newTodo' as a property to the object of 'text'to that
     setNewTodo('');
+    // Here we assign setNewTodo to nothing as information will be entered in at the bottom (line 38)
   };
-
+  
   const handleDelete = (index) => {
     setTodos(todos.filter((_, i) => i !== index));
   };
+
+  //This one here is interesing because and underscore means it's nothing, 
+  //  single underscore is a convention used by some javascript programmers to 
+  // indicate to other programmers that they should "ignore this binding/parameter"
+  // So handleDete is saying that we are passing in an argument of index 
+  // and assigning it to setTodos. What's getting assigned to setTodos? 
+  // we are assigning the variable of todos and removing items that don't match the index
+  // So we are removing everything because no items match, therefore deleted
 
   return (
     <div className="ToDo">
       <h1>Todo List</h1>
       <form onSubmit={handleSubmit}>
         <input
-          type="text"
           value={newTodo}
           onChange={(event) => setNewTodo(event.target.value)}
+
         />
         <button type="submit">Add</button>
       </form>
@@ -30,7 +43,11 @@ function TodoList() {
         {todos.map((todo, index) => (
           <li key={index}>
             {todo.text}
+            {/* note how we have passed in a map with a key. This is a good idea as to keep track in what order things are displayed 
+            and compiled */}
+            {/* Also, note how 'text' is now appearing a again as an object, and we are displaying the value of text from 'NewTodo (line12) */}
             <button onClick={() => handleDelete(index)}>Delete</button>
+            {/* so now the handleDelete function is taking in the argument of index */}
           </li>
         ))}
       </ul>
--- client/src/textAppear.js
+++ client/src/textAppear.js
@@ -6,8 +6,15 @@ export default function TextAppear() {
   return (
     <div className="textAppear">
       <h3>Input Text and it will appear right below this line</h3>
-      <input values={text} onChange={(e) => setText(e.target.value)}></input>
+      <input values={text} onChange={(x) => setText(x.target.value)}></input>
       <h1>{text}</h1>
     </div>
+
   );
 }
+// note how 'x' is just an argument that we are passing into the function
+// the most important thing is sthat onChange is actually the event
+
+// 'target' returns the element where the event occurred, and in this case 
+// it would be 'x'. In essence, we are saying. Put anything you want in and it will be reflected outward
+// and we are assigning it to 'setText'. 
